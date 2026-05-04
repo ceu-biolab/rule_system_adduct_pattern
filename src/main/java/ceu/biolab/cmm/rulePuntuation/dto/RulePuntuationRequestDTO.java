f0274be6869bcec31d03dd1751f583e9f6df9c04
Joaquin de Vicente Abad <joaquin.devicenteabad@usp.ceu.es>
2026-05-04 11:37:58 +0200

    Starting point

--- src/main/java/ceu/biolab/cmm/rulePuntuation/dto/RulePuntuationRequestDTO.java
+++ src/main/java/ceu/biolab/cmm/rulePuntuation/dto/RulePuntuationRequestDTO.java
@@ -3,10 +3,12 @@ package ceu.biolab.cmm.rulePuntuation.dto;
 import lombok.Data;
 
 import ceu.biolab.cmm.shared.dto.FeatureAnnotation;
+import ceu.biolab.cmm.shared.domain.MobilePhases;
 
 @Data
 public class RulePuntuationRequestDTO {
     //private String targetCandidate;
     private FeatureAnnotation.AnnotatedFeature feature;
     private double score;
+    private MobilePhases mobilePhase;
 }
